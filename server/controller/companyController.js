1344b64b5efcb6ef8302c4496cd1e2281da16bef
Siriram Hazam <mickykubpom@outlook.co.th>
2024-04-24 23:29:35 +0700

    bcrypt and try catch

--- server/controller/companyController.js
+++ server/controller/companyController.js
@@ -14,49 +14,65 @@ export const createCompany = async (req, res) => {
         return res.json({ status: 400, message: "CompanyCreate: Email Already Taken!!" })
     }
 
-    const newCompany = await prisma.company.create({
-        data: {
-            companyName: companyName,
-            address: address,
-            phone_number: phone_number,
-            email: email,
-            dpo: dpo
-        }
-    });
+    try {
+        const newCompany = await prisma.company.create({
+            data: {
+                companyName: companyName,
+                address: address,
+                phone_number: phone_number,
+                email: email,
+                dpo: dpo
+            }
+        });
 
-    return res.json({ status: 200, data: newCompany, message: "Company Data Created!!" });
+        return res.json({ status: 200, data: newCompany, message: "Company Data Created!!" });
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const updateCompany = async (req, res) => {
     const companyId = req.params.id
     const { companyName, address, phone_number, email, dpo } = req.body;
 
-    await prisma.company.update({
-        where: {
-            id: Number(companyId)
-        },
-        data: {
-            companyName: companyName,
-            address: address,
-            phone_number: phone_number,
-            email: email,
-            dpo: dpo
-        }
-    })
-    return res.json({ status: 200, message: "Company Data Updated!" })
+    try {
+        await prisma.company.update({
+            where: {
+                id: Number(companyId)
+            },
+            data: {
+                companyName: companyName,
+                address: address,
+                phone_number: phone_number,
+                email: email,
+                dpo: dpo
+            }
+        })
+        return res.json({ status: 200, message: "Company Data Updated!" })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const deleteCompany = async (req, res) => {
     const companyId = req.params.id
-    await prisma.company.delete({
-        where: {
-            id: Number(companyId)
-        }
-    })
+    try {
+        await prisma.company.delete({
+            where: {
+                id: Number(companyId)
+            }
+        })
+    } catch (error) {
+        console.error(error.message)
+    }
 
 }
 
 export const fetchCompany = async (req, res) => {
-    const company = await prisma.company.findMany({})
-    return res.json({ status: 200, data: company })
+    try {
+        const company = await prisma.company.findMany({})
+        return res.json({ status: 200, data: company })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,7 @@
 import prisma from "../db/db.config.js";
 
+import bcrypt from "bcrypt"
+
 export const createUser = async (req, res) => {
     const { username, password, fullname, email, role, company_id, edit_time } = req.body
 
@@ -11,75 +13,138 @@ export const createUser = async (req, res) => {
         return res.json({ status: 400, message: "User Create: Email Already Taken!!" })
     }
 
-    const newUser = await prisma.user_account.create({
-        data: {
-            username: username,
-            password: password,
-            fullname: fullname,
-            email: email,
-            role: role,
-            company_id: company_id,
-            edit_time: new Date().toISOString(),
-        }
+    const findUser = await prisma.user_account.findUnique({
+        where: { username: username }
     })
-    return res.json({ status: 200, data: newUser, message: "User Data Created!!" })
+
+    if (findUser) {
+        return res.json({ status: 400, message: "User Create: Username Already Taken!!" })
+    }
+
+    try {
+        const passhash = await bcrypt.hash(password, 10)
+
+        const newUser = await prisma.user_account.create({
+            data: {
+                username: username,
+                password: passhash,
+                fullname: fullname,
+                email: email,
+                role: role,
+                company_id: company_id,
+                edit_time: new Date().toISOString(),
+            }
+        })
+        return res.json({ status: 200, data: newUser, message: "User Data Created!!" })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const updateUser = async (req, res) => {
     const userId = req.params.id
-    const { password, fullname, email, role, company_id, edit_time } = req.body
+    const { password, fullname, role, company_id } = req.body
 
-    await prisma.user_account.update({
-        where: {
-            id: Number(userId)
-        },
-        data: {
-            password: password,
-            fullname: fullname,
-            role: role,
-            company_id: company_id,
-            edit_time: new Date().toISOString(),
-        }
-    })
+    // console.log(password)
 
-    return res.json({ status: 200, message: "User Data Updated!!" })
+    // if (password == undefined) {
+    //     passhash = password
+    // } else {
+    //     const passhash = await bcrypt.hash(password, 10)
+    // }
+
+    try {
+        const passhash = await bcrypt.hash(password, 10)
+
+
+        await prisma.user_account.update({
+            where: {
+                id: Number(userId)
+            },
+            data: {
+                password: passhash,
+                fullname: fullname,
+                role: role,
+                company_id: company_id,
+                edit_time: new Date().toISOString(),
+            }
+        })
+
+        return res.json({ status: 200, message: "User Data Updated!!" })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const deleteUser = async (req, res) => {
     const userId = req.params.id
 
-    await prisma.user_account.delete({
-        where: {
-            id: userId
-        }
-    })
+    try {
+        await prisma.user_account.delete({
+            where: {
+                id: userId
+            }
+        })
 
-    return res.json({ status: 200, message: "User Data Deleted!!" })
-}
-
-export const fecthUserAcc = async (req, res) => {
-    const user = await prisma.user_account.findMany({
-        select: {
-            username: true
-        }
-    })
-    return res.json({ status: 200, data: user })
+        return res.json({ status: 200, message: "User Data Deleted!!" })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const fetchUser = async (req, res) => {
-    const user = await prisma.user_account.findMany({})
+    try {
+        const user = await prisma.user_account.findMany({})
 
-    return res.json({ status: 200, data: user })
+        return res.json({ status: 200, data: user })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 export const fetchUserLogin = async (req, res) => {
-    const { username } = req.body
+    const { username, password, email } = req.body
 
-    const user = await prisma.user_account.findMany({
-        where: {
-            username: username,
+    try {
+        const user = await prisma.user_account.findUnique({
+            where: {
+                username: username,
+                email: email
+            },
+            select: {
+                username: true,
+                password: true,
+                email: true
+            }
+        })
+
+        if (user.username != null) {
+            const passcheck = await bcrypt.compare(password, user.password)
+
+            if (passcheck) {
+                console.log(user.password)
+                console.log(password)
+                console.log(passcheck)
+            }
         }
-    })
 
-    return res.json({ status: 200, data: user })
+        return res.json({ status: 200, data: user })
+
+    } catch (error) {
+        res.json({ status: 400, message: "User Not Found!" })
+        console.error(error.mesage)
+    }
+}
+
+export const fecthUserAcc = async (req, res) => {
+    try {
+        const user = await prisma.user_account.findMany({
+            select: {
+                username: true
+            }
+        })
+        return res.json({ status: 200, data: user })
+    } catch (error) {
+        console.error(error.message)
+    }
 }

297d5d5c2b8b10c03c3b8639e6d12ea2634d555d
Siriram Hazam <mickykubpom@outlook.co.th>
2024-12-21 00:24:05 +0700

    Update Login.js

--- client1/src/pages/Login/Login.js
+++ client1/src/pages/Login/Login.js
@@ -32,103 +32,70 @@ function Login() {
 
   return (
     <>
-      {/* <section className="bg-gradient-to-r from-cyan-500 to-blue-500 w-screen h-screen"> */}
-      <section className="bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 animate-gradient-x w-screen h-screen">
-        {/* <section className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-screen h-screen"> */}
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            // href="#"
-            className="flex items-center mb-6 text-3xl font-semibold text-gray-900 dark:text-white"
-          >
-            {/* <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            /> */}
-            {/* Welcome to Record of Processing Web App */}
-            <TextScramble
-              text="Welcome to Record of Processing Web App"
-              duration={70}
-            />
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
-              </h1>
-              <form
-                className="space-y-4 md:space-y-6"
-                // action="#"
-                onSubmit={handleSubmit}
-              >
-                <div>
-                  <label
-                    // for="username"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Username
-                  </label>
-                  <input
-                    type="username"
-                    // name="username"
-                    id="username"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="abcdefg...."
-                    required=""
-                    onChange={(e) => setUsename(e.target.value)}
-                  />
-                </div>
-                <div>
-                  <label
-                    // for="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    // name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                {/* <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        for="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div> */}
-                <button
-                  type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 animate-gradient-x focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+      <div className="flex flex-col md:flex-row h-screen">
+        {/* Left cream-colored section */}
+        <div className="hidden md:flex md:w-1/2 bg-[#FFF8E7] dark:bg-gray-800 items-center justify-center">
+          {/* Center content container */}
+          <div className="text-3xl font-semibold text-gray-800 text-center px-8">
+            {/* Welcome Back */}
+            <a className="flex items-center justify-center mb-6 text-5xl font-semibold text-gray-900 dark:text-white">
+              <TextScramble
+                text="Welcome to Record of Processing"
+                duration={70}
+              />
+            </a>
+          </div>
+        </div>
+
+        <section className="bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 animate-gradient-x w-screen h-screen">
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+                </h1>
+                <form
+                  className="space-y-4 md:space-y-6"
+                  // action="#"
+                  onSubmit={handleSubmit}
                 >
-                  Sign in
-                </button>
-                {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <div>
+                    <label
+                      // for="username"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Username
+                    </label>
+                    <input
+                      type="username"
+                      // name="username"
+                      id="username"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="abcdefg...."
+                      required=""
+                      onChange={(e) => setUsename(e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 animate-gradient-x hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all duration-300 transform hover:scale-105"
+                  >
+                    Sign in
+                  </button>
+                  {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
                   <a
                     href="#"
@@ -137,12 +104,13 @@ function Login() {
                     Sign up
                   </a>
                 </p> */}
-              </form>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <ToastContainer />
+        </section>
+        <ToastContainer />
+      </div>
     </>
   );
 }

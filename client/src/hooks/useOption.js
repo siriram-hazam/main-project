503a0c7160784bc0481b9af275a4ca471981900d
Siriram Hazam <mickykubpom@outlook.co.th>
2025-01-12 23:09:23 +0700

    update credential

--- client/src/hooks/useOption.js
+++ client/src/hooks/useOption.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://20.255.153.125:3001"; // URL หลักของ API
+axios.defaults.withCredentials = true; // เปิดการส่ง Cookies
+axios.defaults.headers.common["Content-Type"] = "application/json"; // Content-Type เริ่มต้น
+axios.defaults.headers.common["Accept"] = "application/json"; // Accept header
 
 const optionAllDropdown = async () => {
   try {
--- server/index.js
+++ server/index.js
@@ -13,9 +13,6 @@ app.use(
     origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Range", "X-Content-Range"],
-    maxAge: 600, // Cache preflight requests for 10 minutes
   })
 );
 
